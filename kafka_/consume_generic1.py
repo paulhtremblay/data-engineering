7e43363e883edadebc325af63daadd622c3a0a4f
Henry <paulhtremblay@gmail.com>
2024-08-26 14:24:43 -0700

    adding avsc

--- kafka_/consume_generic1.py
+++ kafka_/consume_generic1.py
@@ -9,9 +9,24 @@ from avro.io import DatumReader
 import avro.schema
 from avro.io import DatumWriter
 
-schema_path = "words.avsc"
-schema = avro.schema.parse(open(schema_path).read())
-READER = DatumReader(schema)
+SCHEMA_PATH_WORDS = "words.avsc"
+SCHEMA_WORDS = avro.schema.parse(open(SCHEMA_PATH_WORDS).read())
+READER_WORDS = DatumReader(SCHEMA_WORDS)
+
+SCHEMA_PATH_NUMBERS = "numbers.avsc"
+SCHEMA_NUMBERS = avro.schema.parse(open(SCHEMA_PATH_NUMBERS).read())
+READER_NUMBERS = DatumReader(SCHEMA_NUMBERS)
+
+SCHEMA_PATH_PERSON_MIDDLE = "person_middle.avsc"
+SCHEMA_PERSON_MIDDLE = avro.schema.parse(open(SCHEMA_PATH_PERSON_MIDDLE).read())
+READER_PERSON_MIDDLE = DatumReader(SCHEMA_PERSON_MIDDLE)
+
+SCHEMA_PATH_PERSON_NO_MIDDLE = "person_no_middle.avsc"
+SCHEMA_PERSON_NO_MIDDLE = avro.schema.parse(open(SCHEMA_PATH_PERSON_NO_MIDDLE).read())
+READER_PERSON_NO_MIDDLE = DatumReader(SCHEMA_PERSON_NO_MIDDLE)
+
+print(type(READER_WORDS))
+print(type(READER_PERSON_NO_MIDDLE))
 
 def _get_args():
     parser = argparse.ArgumentParser()
@@ -27,23 +42,49 @@ def _get_args():
             choices = ['avro','json'],  
             default = 'json',
             help="deserializer")
+    parser.add_argument("--schema", 
+            choices = ['words','numbers', 
+                'person-no-middle', 
+                'person-middle'],  
+            help="type for deserializer")
     args = parser.parse_args()
     return args
 
-def decode(msg_value):
-    message_bytes = io.BytesIO(msg_value)
-    message_bytes.seek(0)
-    decoder = BinaryDecoder(message_bytes)
-    event_dict = READER.read(decoder)
-    return event_dict
+def get_decode(schema, verbose):
+    if verbose:
+        print(f'schema type is {schema}')
+    def wrapper(msg_value):
+        if schema == 'words':
+            reader = READER_WORDS
+        elif schema == 'numbers':
+            reader = READER_NUMBERS
+        elif schema == 'person-no-middle':
+            reader = READER_PERSON_NO_MIDDLE
+        elif schema == 'person-middle':
+            reader = READER_PERSON_MIDDLE
+        else:
+            raise ValueError('no match')
+        message_bytes = io.BytesIO(msg_value)
+        message_bytes.seek(0)
+        decoder = BinaryDecoder(message_bytes)
+        event_dict = reader.read(decoder)
+        return event_dict
+    if verbose:
+        print(f'wrapper is {wrapper}')
+    return wrapper
 
 def get_consumer(topic_name, group_id, 
         deserializer,
-        enable_auto_commit = True):
+        schema,
+        enable_auto_commit = True,
+        verbose = False):
     if deserializer == 'json':
         value_deserializer=lambda x: loads(x.decode('utf-8'))
     else:
+        decode = get_decode(schema = schema, verbose = verbose)
         value_deserializer = decode
+    if verbose:
+        print(f'decode is {decode}')
     consumer = KafkaConsumer(
         topic_name,
          bootstrap_servers=['localhost:9092'],
@@ -65,16 +106,23 @@ def get_data_(consumer):
         print(message)
 
 def main(topic_name, group_id, 
-        deserializer, verbose = False):
+        deserializer, schema = None, 
+        verbose = False):
     consumer = get_consumer(
             topic_name = topic_name,
             deserializer = deserializer,
-            group_id = group_id)
+            group_id = group_id,
+            schema = schema,
+            verbose = verbose
+            )
     get_data(consumer = consumer)
 
 if __name__ == '__main__':
     args = _get_args()
     main(topic_name = args.topic,
             deserializer = args.deserializer,
-            group_id = args.group_id)
+            group_id = args.group_id,
+            schema = args.schema,
+            verbose = args.verbose
+            )
 
--- kafka_/producer_generic1.py
+++ kafka_/producer_generic1.py
@@ -7,6 +7,7 @@ import avro.schema
 from avro.io import DatumWriter
 import io
 from datetime import timezone
+import random
 
 from avro.io import DatumWriter
 
@@ -24,8 +25,13 @@ def _get_args():
             type = str,  
             required = True,
             help="name of topic")
+    parser.add_argument("--key", 
+            type = str,  
+            required = False,
+            default = 'key1',
+            help="name of key")
     parser.add_argument("--data", 
-            choices = ['words','numbers'],  
+            choices = ['words','numbers', 'person'],  
             default = 'words',
             help="func")
     parser.add_argument("number", 
@@ -43,42 +49,57 @@ def produce(args):
     if args.serializer == 'json':
         producer = get_producer_json()
     elif args.serializer == 'avro':
-        producer = get_producer_avro()
+        producer = get_producer_avro(type_ = args.data)
     gen = None
     if args.data == 'words':
         func = make_words
     elif args.data == 'numbers':
         gen = num_gen()
         func = make_numbers
+
+    elif args.data == 'person':
+        func = make_person_middle
+    def wrapper():
+        func(producer = producer,
+                topic_name = args.topic_name,
+                gen = gen,
+                key = args.key)
+        time.sleep(args.sleep_time)
     if not args.number:
         while True:
-            func(producer = producer,
-                    topic_name = args.topic_name,
-                    gen = gen)
-            time.sleep(args.sleep_time)
+            wrapper()
     else:
         for i in range(args.number):
-            func(producer = producer,
-                    topic_name = args.topic_name,
-                    gen = gen)
-            time.sleep(args.sleep_time)
+            wrapper()
 
 
-def make_words(producer, topic_name, *args, **kwargs):
+def make_words(producer, topic_name, key, *args, **kwargs):
     words = ['tiger', 'bear']
     x = datetime.datetime.now().astimezone()
     for j in words:
         data = {'word' : j,
                 'timestamp': x}
-        producer.send(topic_name, value=data)
+        producer.send(topic = topic_name, value=data, 
+                key = key.encode('utf8'))
 
 def num_gen():
     for i in range(100000):
         yield i
 
-def make_numbers(producer, topic_name, gen):
-        data = {'number' : next(gen)}
-        producer.send(topic_name, value=data)
+def make_numbers(producer, topic_name, gen, key):
+    data = {'number' : next(gen)}
+    producer.send(topic_name, 
+            value=data,
+            key = key.encode('utf8'))
+
+def make_person_middle(producer, topic_name, gen, key):
+    data = {'first' : 'Paul',
+            'last': 'Tremblay',
+            'middle': 'Henry',
+            }
+    producer.send(topic_name, 
+            value=data,
+            key = key.encode('utf8'))
 
 def get_producer_json():
     producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
@@ -87,21 +108,30 @@ def get_producer_json():
                 )
     return producer
 
-def avro_serializer(the_dict):
-    schema_path = "words.avsc"
-    schema = avro.schema.parse(open(schema_path).read())
-    writer = DatumWriter(schema)
-    bytes_writer = io.BytesIO()
-    encoder = avro.io.BinaryEncoder(bytes_writer)
-    writer.write(the_dict, encoder)
-    raw_bytes = bytes_writer.getvalue()
-    return raw_bytes
+def avro_serializer(type_):
+    def wrapper(the_dict):
+        if type_ == 'words':
+            schema_path = 'words.avsc'
+        elif type_ == 'numbers':
+            schema_path = 'numbers.avsc'
+        elif type_ == 'person':
+            schema_path = 'person_middle.avsc'
+        else:
+            raise ValueError('no matching schema')
 
+        schema = avro.schema.parse(open(schema_path).read())
+        writer = DatumWriter(schema)
+        bytes_writer = io.BytesIO()
+        encoder = avro.io.BinaryEncoder(bytes_writer)
+        writer.write(the_dict, encoder)
+        raw_bytes = bytes_writer.getvalue()
+        return raw_bytes
+    return wrapper
 
-def get_producer_avro():
-
+def get_producer_avro(type_):
+    func = avro_serializer(type_)
     producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
-                value_serializer=avro_serializer,
+                value_serializer=func,
                 acks = 'all'
                 )
     return producer
