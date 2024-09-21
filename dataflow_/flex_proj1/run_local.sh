d05b43436e40cff7e3fb44f61977070916d47634
Henry <paulhtremblay@gmail.com>
2024-09-21 13:27:36 -0700

    fixing local and typo

--- dataflow_/flex_proj1/run_local.sh
+++ dataflow_/flex_proj1/run_local.sh
@@ -1,10 +1,9 @@
 set -e
 source envs.sh
 
-OUTPUT_FILE=gs://paul-henry-tremblay-general/kinglear_out_flex1.txt
+OUTPUT_FILE=gs://paul-henry-tremblay-general/flex_local/kinglear_out_flex-
 INPUT_FILE=gs://paul-henry-tremblay-general/kinglear.txt
 TEMP_LOCATION=gs://paul-henry-tremblay-general/temp
-echo $IMAGE_URL
 
 python  wordcount.py \
   --input=$INPUT_FILE \
@@ -12,9 +11,6 @@ python  wordcount.py \
   --project=$PROJECT_ID \
   --region=$REGION \
   --temp_location=$TEMP_LOCATION \
-  --runner=DataflowRunner \
-  --experiments=use_runner_v2 \
-  --sdk_container_image=$IMAGE_URL \
-  --sdk_location=container 
+  --runner=DirectRunner \
 
 
