fc75b4e820af64adff2f9ca5c5aff6b17b3d0d33
henry <henry.tremblay@paccar.com>
2024-10-01 20:19:40 +0000

    adding vim

---
+++ personal/vim/init.sh
@@ -0,0 +1,3 @@
+mkdir -p ~/.vim/pack/vendor/start
+git clone git@github.com:ap/vim-buftabline.git  ~/.vim/pack/vendor/start/vim-buftab-master
+git clone https://github.com/ervandew/supertab.git ~/.vim/pack/vendor/start/supertab-master
