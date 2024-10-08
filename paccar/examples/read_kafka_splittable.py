39ec1949b2f054bbb518bd2b7adf6088538b48df
henry <henry.tremblay@paccar.com>
2024-10-08 00:21:48 +0000

    cleanup

--- paccar/examples/read_kafka_splittable.py
+++ paccar/examples/read_kafka_splittable.py
@@ -33,26 +33,6 @@ python publish_pub_sub.py ingest_test
 """
 
 
-def _get_args() -> (argparse.Namespace, list):
-    parser = argparse.ArgumentParser()
-    parser.add_argument( '--project',
-             required = False,
-             default = 'paccar-trusted-data-nonprod',
-             help='project ')
-    parser.add_argument( '--runner',
-             choices = ['DataflowRunner', 'DirectRunner', 'FlinkRunner'],
-             default = 'DirectRunner',
-             help='runner to use ')
-    parser.add_argument(
-          '--verbose',
-          type = int,
-          required=False,
-          default = 0,
-          help='verbose level, with 0 being not messages at all.')
-
-    known_args, pipeline_args = parser.parse_known_args()
-    return known_args, pipeline_args
-
 
 class GroupMessagesByFixedWindows(PTransform):
     """A composite transform that groups Kafka messages based on publish time
@@ -77,21 +57,8 @@ class GroupMessagesByFixedWindows(PTransform):
 
 
 
-def json_serial(obj):
-    """JSON serializer for objects not serializable by default json code"""
 
-    if isinstance(obj, (datetime, date)):
-        return obj.isoformat()
-    raise TypeError ("Type %s not serializable" % type(obj))
-
-def _dump_to_json(element:object):
-    """
-    for debugging purposes
-    """
-    return json.dumps(element, default = json_serial)
-
-
-class FileToWordsRestrictionProvider(beam.transforms.core.RestrictionProvider
+class MyRestrictionProvider(beam.transforms.core.RestrictionProvider
                                      ):
 
     def initial_restriction(self, list_):
@@ -108,50 +75,34 @@ class SplitFn(beam.DoFn):
     def __init__(self, split_size = 1):
         self.split_size = split_size
 
+    def start_bundle(self, *args, **kwargs):
+        self.start_bundle = datetime.now()
+
+    def setup(self):
+        self.connection = 1
+
     def process(
         self,
-        list_,
-        tracker=beam.DoFn.RestrictionParam(FileToWordsRestrictionProvider())):
+        element,
+        tracker=beam.DoFn.RestrictionParam(MyRestrictionProvider())):
         counter = 0
         while tracker.try_claim(counter):
-            yield get_value(list_[counter: counter + self.split_size])
+            yield self.write_to_snowflake(element[counter: counter + self.split_size])
             counter += self.split_size
 
-def get_value(list_):
-    l = []
-    for i in list_:
-        l.append(i.value)
-    return l
+    def write_to_snowflake(self, list_):
+        """
+        write to snowflake here
+        """
+        self.connection #this is already defeined 
+        l = []
+        for i in list_:
+            #actually write here
+            l.append(i.value)
+        return l
 
 def just_list(element):
     return [element]
-
-
-def _make_pipeline_options(known_args:argparse.Namespace) -> apache_beam.options.pipeline_options.PipelineOptions:
-    """
-    create pipeline_options needed for pipeline
-    TODO: this probabably could be abstracted and re-used
-
-    :param: known_args, ??: arguments from command line (but not run time)
-    :returns: apache_beam.options.pipeline_options.PipelineOptions: a class for runtime options
-    """
-    if known_args.runner == 'FlinkRunner':
-        pipeline_options = PipelineOptions(
-            region= 'us-westl',
-            project= known_args.project, 
-            runner= known_args.runner,
-            flink_master="localhost:8081",
-            environment_type="LOOPBACK",
-            streaming=True, 
-        )
-    elif known_args.runner in ['DirectRunner', 'DataflowRunner']:
-        pipeline_options = PipelineOptions(
-            region= 'us-westl',
-            project= known_args.project, 
-            runner= known_args.runner,
-            streaming=True, 
-        )
-    return pipeline_options
 
 
 def run(window_size:float=10.0, num_shards:int=5):
@@ -159,8 +110,11 @@ def run(window_size:float=10.0, num_shards:int=5):
     main entry point to run Apache Beam Job
 
     """
-    known_args, pipeline_args = _get_args()
-    pipeline_options = _make_pipeline_options(known_args)
+    pipeline_options = PipelineOptions(
+        region= 'us-westl',
+        runner= 'DirectRunner',
+        streaming=True, 
+        )
     pipeline_options.view_as(SetupOptions).save_main_session = True
     max_num_records = 10
     with Pipeline(options=pipeline_options) as pipeline:
