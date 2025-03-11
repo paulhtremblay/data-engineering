0c1fe90ec7a6fb27f419b5fed9d9aef35393bbc7
Henry <henry.tremblay@krusteaz.com>
2025-03-11 09:36:35 -0700

    adding exaple in argparse

--- examples/args_ex1.py
+++ examples/args_ex1.py
@@ -9,6 +9,10 @@ def _custom_type(s):
         return int(s)
     raise argparse.ArgumentTypeError('Value has to be between 0 and 1')
 
+def _get_path(s):
+    if not os.path.isfile(s):
+        raise argparse.ArgumentTypeError(f'{s} not a path')
+
 
 
 def _get_args():
@@ -17,6 +21,7 @@ def _get_args():
     parser.add_argument("paths", nargs='+', help="path of file")
     parser.add_argument("--out", '-o',  required = True, help="out-path")
     parser.add_argument('--number', type = _custom_type)
+    parser.add_argument('--path', type = _get_path)
     args = parser.parse_args()
     return args
 
